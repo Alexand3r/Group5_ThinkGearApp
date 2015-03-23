d3b55e8c7e7e63080e3c2bbe3c8d35612ef93a76
Alexand3r <andrei.tudor27@yahoo.com>
2015-03-23 09:49:47 +0100

    Headset controls the car through Attention Variable + Removed AndEngine Game Tutorial Andrei

--- app/src/main/java/g5/mindwave/OneToRuleTHEMALL.java
+++ app/src/main/java/g5/mindwave/OneToRuleTHEMALL.java
@@ -1,14 +1,18 @@
 package g5.mindwave;
 
+import android.bluetooth.BluetoothAdapter;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import com.neurosky.thinkgear.TGDevice;
+import com.neurosky.thinkgear.TGEegPower;
+
 import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
-import org.andengine.entity.IEntity;
-import org.andengine.entity.modifier.IEntityModifier;
-import org.andengine.entity.modifier.LoopEntityModifier;
-import org.andengine.entity.modifier.RotationModifier;
-import org.andengine.entity.modifier.SequenceEntityModifier;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.opengl.texture.ITexture;
@@ -18,8 +22,6 @@ import org.andengine.opengl.texture.region.TextureRegionFactory;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
 import org.andengine.util.adt.io.in.IInputStreamOpener;
 import org.andengine.util.debug.Debug;
-import org.andengine.util.modifier.IModifier;
-import org.andengine.util.modifier.LoopModifier;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -28,11 +30,19 @@ import java.io.InputStream;
  * Created by Andrei on 3/20/2015.
  */
 public class OneToRuleTHEMALL extends SimpleBaseGameActivity{
-
+    TGDevice tgDevice;
+    BluetoothAdapter btAdapter;
+    Sprite carSprite;
+    Sprite wheelSprite1;
+    Sprite wheelSprite2;
     private static int CAMERA_WIDTH = 800;
     private static int CAMERA_HEIGHT = 480;
     private ITextureRegion mBackgroundTextureRegion,mCarTextureRegion,mWheel1,mWheel2;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+      //  setContentView(R.layout.activity_main);
 
+    }
     @Override
     protected void onCreateResources() throws IOException {
        try {
@@ -72,88 +82,75 @@ public class OneToRuleTHEMALL extends SimpleBaseGameActivity{
         final Scene scene = new Scene();
         Sprite backgroundSprite = new Sprite(400,240, this.mBackgroundTextureRegion,getVertexBufferObjectManager());
         scene.attachChild(backgroundSprite);
-        Sprite carSprite = new Sprite(200,100, this.mCarTextureRegion,getVertexBufferObjectManager());
+        carSprite = new Sprite(200,100, this.mCarTextureRegion,getVertexBufferObjectManager());
         scene.attachChild(carSprite);
-        final Sprite wheelSprite1 = new Sprite(120,68,this.mWheel1,getVertexBufferObjectManager());
+        wheelSprite1 = new Sprite(120,68,this.mWheel1,getVertexBufferObjectManager());
 
-        Sprite wheelSprite2 = new Sprite(295,70,this.mWheel1,getVertexBufferObjectManager());
+        wheelSprite2 = new Sprite(295,70,this.mWheel1,getVertexBufferObjectManager());
         scene.attachChild(wheelSprite1);
         scene.attachChild(wheelSprite2);
-        RotationModifier rotMod = new RotationModifier(1f, 0, 980);
 
-        SequenceEntityModifier s1 =new SequenceEntityModifier(
 
-                new RotationModifier(3, 0, 360)
-                //new AlphaModifier(2, 1, 0),
-                //new AlphaModifier(1, 0, 1),
-                //new ScaleModifier(2, 1, 0.5f),
-                //new DelayModifier(0.5f)
-
-                //new ParallelEntityModifier(
-                //new ScaleModifier(3, 0.5f, 5),
-                //		new RotationByModifier(3, 360)
-                //),
-                //new ParallelEntityModifier(
-                //new ScaleModifier(3, 5, 1),
-                //		new RotationModifier(3, 360, 0)
-                //)
-        );
-        final LoopEntityModifier entityModifier =
-                new LoopEntityModifier(
-                        new IEntityModifier.IEntityModifierListener() {
-                            @Override
-                            public void onModifierStarted(final IModifier<IEntity> pModifier, final IEntity pItem) {
-                                new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        //Toast.makeText(GameScene.this, "Sequence started.", Toast.LENGTH_SHORT).show();
-                                    }
-                                };
-                            }
-
-                            @Override
-                            public void onModifierFinished(final IModifier<IEntity> pEntityModifier, final IEntity pEntity) {
-                                new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        //Toast.makeText(GameScene.this, "Sequence finished.", Toast.LENGTH_SHORT).show();
-                                    }
-                                };
-                            }
-                        }
-                        ,
-                        999,
-                        new LoopEntityModifier.ILoopEntityModifierListener() {
-                            @Override
-                            public void onLoopStarted(final LoopModifier<IEntity> pLoopModifier, final int pLoop, final int pLoopCount) {
-                                new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        //Toast.makeText(GameScene.this, "Loop: '" + (pLoop + 1) + "' of '" + pLoopCount + "' started.", Toast.LENGTH_SHORT).show();
-                                    }
-                                };
-                            }
-
-                            @Override
-                            public void onLoopFinished(final LoopModifier<IEntity> pLoopModifier, final int pLoop, final int pLoopCount) {
-                                new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        //Toast.makeText(GameScene.this, "Loop: '" + (pLoop + 1) + "' of '" + pLoopCount + "' finished.", Toast.LENGTH_SHORT).show();
-                                    }
-                                };
-                            }
-                        },
-                        s1
-                );
-        wheelSprite2.registerEntityModifier(s1);
-        wheelSprite1.registerEntityModifier(s1);
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        Log.v("BT:",btAdapter.getName());
+        if(btAdapter != null) { tgDevice = new TGDevice(btAdapter, handler);
+            tgDevice.connect(true);
+            Log.v("Device",""+tgDevice.getState());
+        }
         return scene;
     }
+    private final Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) { switch (msg.what) {
+            case TGDevice.MSG_STATE_CHANGE:
+                Log.v("HelloEEG","State changed");
+                switch (msg.arg1) {
+                    case TGDevice.STATE_IDLE: break;
+                    case TGDevice.STATE_CONNECTING:
+                        Log.v("HelloEEG","Connecting");
+                        break;
+                    case TGDevice.STATE_CONNECTED: tgDevice.start();
+                        Log.v("HelloEEG","Connected");
+                        break;
+                    case TGDevice.STATE_DISCONNECTED:
+                        break;
+                    case TGDevice.STATE_NOT_FOUND: case TGDevice.STATE_NOT_PAIRED: default:
+                        break;
 
+                } break; case TGDevice.MSG_POOR_SIGNAL:
+                // Log.v("HelloEEG", "PoorSignal: " + msg.arg1);
+
+            case TGDevice.MSG_ATTENTION:
+                Log.v("HelloEEG", "Attention: " + msg.arg1);
+
+                if(carSprite!=null&&wheelSprite1!=null&&wheelSprite2!=null)
+                {   wheelSprite1.setPosition(msg.arg1*5-80,68);
+                    wheelSprite2.setPosition(msg.arg1*5+95,70);
+                    carSprite.setPosition(msg.arg1*5,100);}
+                break;
+            case TGDevice.MSG_BLINK:
+                Log.v("HelloEEG", "Blinks:" + msg.arg1);
+                break;
+            case TGDevice.MSG_MEDITATION:
+                Log.v("HelloEEG", "Meditation:" +msg.arg1);
+                //  progressMeditation.setProgress(msg.arg1);
+                break;
+            case TGDevice.MSG_RAW_DATA:
+                // int rawValue = msg.arg1;
+                // Log.v("HelloEEG", "Raw Data: "+ rawValue);
+                break;
+            case TGDevice.MSG_EEG_POWER:
+
+                TGEegPower ep = (TGEegPower)msg.obj;
+                Log.v("HelloEEG", "Delta: " + ep.delta); default:
+                break;
+        }
+        }
+    };
     @Override
     public EngineOptions onCreateEngineOptions() {
         final Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
         return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new FillResolutionPolicy(), camera);
+
     }
 }
