2c9b8fe7299866294db1a86eed8734477468fb43
Alexand3r <andrei.tudor27@yahoo.com>
2015-03-23 20:53:34 +0100

    Physics world now act as they should + new wheel that is round - Andrei

--- app/src/main/java/g5/mindwave/OneToRuleTHEMALL.java
+++ app/src/main/java/g5/mindwave/OneToRuleTHEMALL.java
@@ -9,27 +9,31 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.joints.RevoluteJoint;
+import com.badlogic.gdx.physics.box2d.joints.RevoluteJointDef;
 import com.neurosky.thinkgear.TGDevice;
 import com.neurosky.thinkgear.TGEegPower;
 
 import org.andengine.engine.camera.Camera;
+import org.andengine.engine.handler.IUpdateHandler;
 import org.andengine.engine.handler.physics.PhysicsHandler;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
-import org.andengine.entity.IEntity;
-import org.andengine.entity.modifier.IEntityModifier;
-import org.andengine.entity.modifier.LoopEntityModifier;
-import org.andengine.entity.modifier.RotationModifier;
-import org.andengine.entity.modifier.SequenceEntityModifier;
 import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.Background;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.entity.util.FPSLogger;
+import org.andengine.extension.physics.box2d.PhysicsConnector;
 import org.andengine.extension.physics.box2d.PhysicsFactory;
 import org.andengine.extension.physics.box2d.PhysicsWorld;
+import org.andengine.extension.physics.box2d.util.Vector2Pool;
+import org.andengine.input.sensor.acceleration.AccelerationData;
+import org.andengine.input.sensor.acceleration.IAccelerationListener;
 import org.andengine.opengl.texture.ITexture;
 import org.andengine.opengl.texture.bitmap.BitmapTexture;
 import org.andengine.opengl.texture.region.ITextureRegion;
@@ -37,8 +41,6 @@ import org.andengine.opengl.texture.region.TextureRegionFactory;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
 import org.andengine.util.adt.io.in.IInputStreamOpener;
 import org.andengine.util.debug.Debug;
-import org.andengine.util.modifier.IModifier;
-import org.andengine.util.modifier.LoopModifier;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -46,7 +48,7 @@ import java.io.InputStream;
 /**
  * Created by Andrei on 3/20/2015.
  */
-public class OneToRuleTHEMALL extends SimpleBaseGameActivity{
+public class OneToRuleTHEMALL extends SimpleBaseGameActivity implements IAccelerationListener{
     TGDevice tgDevice;
     BluetoothAdapter btAdapter;
     Camera camera;
@@ -54,16 +56,22 @@ public class OneToRuleTHEMALL extends SimpleBaseGameActivity{
     Sprite carSprite;
     Sprite wheelSprite1;
     Sprite wheelSprite2;
-    PhysicsHandler carphysicsHandler;
+    Body wheelBody1,wheelBody2,carBody;
+
+    static RevoluteJoint rj1;
+    static RevoluteJoint rj2;
+
+    PhysicsHandler carPhysicsHandler;
 
     private PhysicsWorld mPhysicsWorld;
-
+    public static final float PIXEL_TO_METER_RATIO_DEFAULT = 32.0f;
     private static int CAMERA_WIDTH = 800;
     private static int CAMERA_HEIGHT = 480;
-    private static final FixtureDef FIXTURE_DEF = PhysicsFactory.createFixtureDef(1, 0.5f, 0.5f);
+    private static FixtureDef FIXTURE_DEF = PhysicsFactory.createFixtureDef(10f, 0.0f, 0.2f);
+
     int x=0;
 
-    private ITextureRegion mBackgroundTextureRegion,mCarTextureRegion,mWheel1,mWheel2;
+    private ITextureRegion mBackgroundTextureRegion,mCarTextureRegion,mWheel1;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
       //  setContentView(R.layout.activity_main);
@@ -84,7 +92,7 @@ public class OneToRuleTHEMALL extends SimpleBaseGameActivity{
            ITexture carTexture = new BitmapTexture(this.getTextureManager(), new IInputStreamOpener() {
                @Override
                public InputStream open() throws IOException {
-                   return getAssets().open("gfx/car2.png");
+                   return getAssets().open("gfx/car3.png");
                }
            });
            carTexture.load();
@@ -93,7 +101,7 @@ public class OneToRuleTHEMALL extends SimpleBaseGameActivity{
            ITexture wheel1Texture = new BitmapTexture(this.getTextureManager(), new IInputStreamOpener() {
                @Override
                public InputStream open() throws IOException {
-                   return getAssets().open("gfx/wheel.png");
+                   return getAssets().open("gfx/roata.png");
                }
            });
            wheel1Texture.load();
@@ -106,103 +114,110 @@ public class OneToRuleTHEMALL extends SimpleBaseGameActivity{
 
     @Override
      public  Scene onCreateScene() {
+
+        //Engine Options
+
         this.mEngine.registerUpdateHandler(new FPSLogger());
+        //Scene
+
         scene = new Scene();
+        scene.setBackground(new Background(0, 0, 0));
+
+        //Physics
 
         this.mPhysicsWorld = new PhysicsWorld(new Vector2(0, SensorManager.GRAVITY_EARTH), false);
-        final Rectangle ground = new Rectangle(0, CAMERA_HEIGHT - 2, CAMERA_WIDTH, 2, getVertexBufferObjectManager());
+        final Rectangle ground = new Rectangle(400, 30 ,160000.0f, 2, getVertexBufferObjectManager());
+        final Rectangle wall =  new Rectangle(1500,0,100,2000,getVertexBufferObjectManager());
         final FixtureDef wallFixtureDef = PhysicsFactory.createFixtureDef(0, 0.5f, 0.5f);
         PhysicsFactory.createBoxBody(this.mPhysicsWorld, ground, BodyDef.BodyType.StaticBody, wallFixtureDef);
+       // PhysicsFactory.createBoxBody(this.mPhysicsWorld, wall, BodyDef.BodyType.StaticBody, wallFixtureDef);
+        scene.attachChild(wall);
         scene.attachChild(ground);
+
+
+        //Logo
 
         Sprite backgroundSprite = new Sprite(400,240, this.mBackgroundTextureRegion,getVertexBufferObjectManager());
         scene.attachChild(backgroundSprite);
+        //Sprite backgroundSprite1 = new Sprite(800,240, this.mBackgroundTextureRegion,getVertexBufferObjectManager());
+        //scene.attachChild(backgroundSprite1);
+
+        //Car Body
+
         carSprite = new Sprite(200,100, this.mCarTextureRegion,getVertexBufferObjectManager());
         scene.attachChild(carSprite);
-        wheelSprite1 = new Sprite(120,68,this.mWheel1,getVertexBufferObjectManager());
 
-        wheelSprite2 = new Sprite(295,70,this.mWheel1,getVertexBufferObjectManager());
+        carBody=PhysicsFactory.createBoxBody(this.mPhysicsWorld,carSprite, BodyDef.BodyType.DynamicBody,FIXTURE_DEF);
+        mPhysicsWorld.registerPhysicsConnector(new PhysicsConnector(carSprite,carBody,true,true));
+        //Car Wheels
+
+
+        wheelSprite1 = new Sprite(118,53,this.mWheel1,getVertexBufferObjectManager());
+        wheelBody1= PhysicsFactory.createCircleBody(this.mPhysicsWorld, wheelSprite1, BodyDef.BodyType.DynamicBody, FIXTURE_DEF);
+        this.mPhysicsWorld.registerPhysicsConnector(new PhysicsConnector(wheelSprite1,wheelBody1));
         scene.attachChild(wheelSprite1);
+
+
+        wheelSprite2 = new Sprite(292,55,this.mWheel1,getVertexBufferObjectManager());
+        wheelBody2= PhysicsFactory.createCircleBody(this.mPhysicsWorld, wheelSprite2, BodyDef.BodyType.DynamicBody, FIXTURE_DEF);
+        this.mPhysicsWorld.registerPhysicsConnector(new PhysicsConnector(wheelSprite2,wheelBody2));
         scene.attachChild(wheelSprite2);
 
 
-        SequenceEntityModifier s1 =new SequenceEntityModifier(
-
-                new RotationModifier(3, 0, 360)
-                //new AlphaModifier(2, 1, 0),
-                //new AlphaModifier(1, 0, 1),
-                //new ScaleModifier(2, 1, 0.5f),
-                //new DelayModifier(0.5f)
-
-                //new ParallelEntityModifier(
-                //new ScaleModifier(3, 0.5f, 5),
-                //		new RotationByModifier(3, 360)
-                //),
-                //new ParallelEntityModifier(
-                //new ScaleModifier(3, 5, 1),
-                //		new RotationModifier(3, 360, 0)
-                //)
-        );
-        final LoopEntityModifier entityModifier =
-                new LoopEntityModifier(
-                        new IEntityModifier.IEntityModifierListener() {
-                            @Override
-                            public void onModifierStarted(final IModifier<IEntity> pModifier, final IEntity pItem) {
-                                new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        //Toast.makeText(GameScene.this, "Sequence started.", Toast.LENGTH_SHORT).show();
-                                    }
-                                };
-                            }
-
-                            @Override
-                            public void onModifierFinished(final IModifier<IEntity> pEntityModifier, final IEntity pEntity) {
-                                new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        //Toast.makeText(GameScene.this, "Sequence finished.", Toast.LENGTH_SHORT).show();
-                                    }
-                                };
-                            }
-                        }
-                        ,
-                        999,
-                        new LoopEntityModifier.ILoopEntityModifierListener() {
-                            @Override
-                            public void onLoopStarted(final LoopModifier<IEntity> pLoopModifier, final int pLoop, final int pLoopCount) {
-                                new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        //Toast.makeText(GameScene.this, "Loop: '" + (pLoop + 1) + "' of '" + pLoopCount + "' started.", Toast.LENGTH_SHORT).show();
-                                    }
-                                };
-                            }
-
-                            @Override
-                            public void onLoopFinished(final LoopModifier<IEntity> pLoopModifier, final int pLoop, final int pLoopCount) {
-                                new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        //Toast.makeText(GameScene.this, "Loop: '" + (pLoop + 1) + "' of '" + pLoopCount + "' finished.", Toast.LENGTH_SHORT).show();
-                                    }
-                                };
-                            }
-                        },
-                        s1
-                );
-        wheelSprite2.registerEntityModifier(s1);
-        wheelSprite1.registerEntityModifier(s1);
-        carphysicsHandler = new PhysicsHandler(carSprite);
-        carSprite.registerUpdateHandler(carphysicsHandler);
+        float x = wheelSprite1.getX();
+        float y = wheelSprite1.getY();
 
 
+
+        //Connect back wheel to th car body using revoluteJoint
+
+        //Roata din spate
+        final RevoluteJointDef revoluteJointDef1 = new RevoluteJointDef();
+        revoluteJointDef1.initialize(wheelBody1, carBody, wheelBody1.getWorldCenter());
+        revoluteJointDef1.enableMotor = false;
+        revoluteJointDef1.motorSpeed = 0;
+        revoluteJointDef1.maxMotorTorque = 100;
+        rj1 = (RevoluteJoint) this.mPhysicsWorld.createJoint(revoluteJointDef1);
+
+        //Connect front wheel to rear axle using revoluteJoint
+
+        final RevoluteJointDef revoluteJointDef2 = new RevoluteJointDef();
+        revoluteJointDef2.initialize(wheelBody2, carBody, wheelBody2.getWorldCenter());
+        revoluteJointDef2.enableMotor = true;
+        revoluteJointDef2.motorSpeed = 0;
+        revoluteJointDef2.maxMotorTorque = 100;
+        rj2 = (RevoluteJoint) this.mPhysicsWorld.createJoint(revoluteJointDef2);
+
+
+        // carphysicsHandler = new PhysicsHandler(carSprite);
+      //  carSprite.registerUpdateHandler(carphysicsHandler);
+
+        //Add Gravity
+        scene.registerUpdateHandler(this.mPhysicsWorld);
+        scene.registerUpdateHandler(new IUpdateHandler() {
+            @Override
+            public void reset() { }
+            @Override
+            public void onUpdate(final float pSecondsElapsed) {
+                if(wall.collidesWith(carSprite)) {
+                   wall.setColor(1, 0, 0);
+                } else {
+                    wall.setColor(0, 1, 0);
+                }
+            }
+        });
+       // this.enableAccelerationSensor(this);
+        final Vector2 gravity = Vector2Pool.obtain(0, -9.8f);
+        this.mPhysicsWorld.setGravity(gravity);
+        //Vector2Pool.recycle(gravity);
+        //ThinkGear Connection
         btAdapter = BluetoothAdapter.getDefaultAdapter();
         Log.v("BT:",btAdapter.getName());
         if(btAdapter != null) { tgDevice = new TGDevice(btAdapter, handler);
             tgDevice.connect(true);
             Log.v("Device",""+tgDevice.getState());
         }
+
         camera.setChaseEntity(carSprite);
         return scene;
     }
@@ -221,6 +236,8 @@ public class OneToRuleTHEMALL extends SimpleBaseGameActivity{
                         break;
                     case TGDevice.STATE_CONNECTED: tgDevice.start();
                         Log.v("HelloEEG","Connected");
+                        //TODO Start the wheels spinning, timer, etc.
+
                         Toast connected = Toast.makeText(getApplicationContext(),"Connected to "+tgDevice.getConnectedDevice().getName(), Toast.LENGTH_SHORT);
                         connected.show();
                         break;
@@ -237,31 +254,14 @@ public class OneToRuleTHEMALL extends SimpleBaseGameActivity{
             case TGDevice.MSG_ATTENTION:
                 Log.v("HelloEEG", "Attention: " + msg.arg1);
 
-                if(carSprite!=null&&wheelSprite1!=null&&wheelSprite2!=null)
-                {
-                    //wheelSprite1.setPosition(msg.arg1*5-80+200,68);
-                    //wheelSprite2.setPosition(msg.arg1*5+95+200,70);
-                   //carphysicsHandler.setAcceleration(msg.arg1, 0);
-                   // tgDevice.stop();
-                   // x=0;
-                    if(msg.arg1!=0) {
-                        if (x <= msg.arg1) {
-                            carphysicsHandler.setAccelerationX(+1);
-                        } else {
-                            carphysicsHandler.setAccelerationX(-1);
-                        }
-                        x = msg.arg1;
 
-                        // carSprite.setPosition(msg.arg1*5+200,100);
-                        //tgDevice.start();
-                    }
-                  }
                 break;
             case TGDevice.MSG_BLINK:
                 //Log.v("HelloEEG", "Blinks:" + msg.arg1);
                 break;
             case TGDevice.MSG_MEDITATION:
-                //Log.v("HelloEEG", "Meditation:" +msg.arg1);
+                Log.v("HelloEEG", "Meditation:" +msg.arg1);
+                rj2.setMotorSpeed(msg.arg1);
                 //  progressMeditation.setProgress(msg.arg1);
                 break;
             case TGDevice.MSG_RAW_DATA:
@@ -279,8 +279,28 @@ public class OneToRuleTHEMALL extends SimpleBaseGameActivity{
     @Override
     public EngineOptions onCreateEngineOptions() {
         camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
-        return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new FillResolutionPolicy(), camera);
+        return new EngineOptions(true, ScreenOrientation.LANDSCAPE_SENSOR, new FillResolutionPolicy(), camera);
 
+    }
+
+    @Override
+    public void onAccelerationAccuracyChanged(AccelerationData pAccelerationData) {
+
+    }
+
+    @Override
+    public void onAccelerationChanged(final AccelerationData pAccelerationData) {
+
+    }
+    @Override
+    public void onResumeGame() {
+        super.onResumeGame();
+        this.enableAccelerationSensor(this);
+    }
+    @Override
+    public void onPauseGame() {
+        super.onPauseGame();
+        this.disableAccelerationSensor();
     }
 
 }
